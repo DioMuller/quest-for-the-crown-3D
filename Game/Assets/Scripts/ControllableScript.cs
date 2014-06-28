f1f9b7ea630a7c1d24a05df8fe110d24c4e55200
Diogo Muller <diogo.muller@live.com>
2014-06-28 17:58:52 -0300

    Initial project with debug scene, for tests.

---
+++ Game/Assets/Scripts/ControllableScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllableScript : MonoBehaviour 
+{
+	public float Speed = 10;
+
+	#region MonoBehaviour Methods
+	/// <summary>
+	/// Behavior initialization.
+	/// </summary>
+	void Start () 
+	{
+	
+	}
+	
+	/// <summary>
+	/// Called every frame.
+	/// </summary>
+	void Update () 
+	{
+		Vector2 axis = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		axis.Normalize();
+		axis *= Speed * Time.deltaTime;
+
+		transform.position += new Vector3 (axis.x, 0, axis.y);
+	}
+	#endregion MonoBehaviour Methods
+}
