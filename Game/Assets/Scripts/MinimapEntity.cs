a5a62d56947889cc45f111d233a46e8eef52a0aa
Diogo Muller <diogo.muller@live.com>
2015-01-03 16:47:00 -0200

    Minimap Entity.

---
+++ Game/Assets/Scripts/MinimapEntity.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+
+public class MinimapEntity : MonoBehaviour 
+{
+	#region Public Attributes
+	/// <summary>
+	/// Minimap sprite.
+	/// </summary>
+	public Texture2D MinimapSprite;
+
+	/// <summary>
+	/// Sprite Color.
+	/// </summary>
+	public Color SpriteColor = Color.white;
+	#endregion Public Attributes
+
+	#region MonoBehaviour Methods
+	/// <summary>
+	/// Initializes MonoBehaviour.
+	/// </summary>
+	void Start () 
+	{
+		// Create GameObject.
+		GameObject minimap = new GameObject();
+		minimap.transform.name = "MinimapSprite";
+		minimap.layer = LayerMask.NameToLayer("Minimap");
+
+		// Create Sprite.
+		var sprite = Sprite.Create(MinimapSprite,
+			new Rect(0, 0, MinimapSprite.width, MinimapSprite.height),
+			new Vector2(0.5f, 0.5f));
+
+		// Create SpriteRenderer Component.
+		minimap.AddComponent<SpriteRenderer>();
+		var component = minimap.GetComponent<SpriteRenderer>();
+
+		component.sprite = sprite;
+		component.color = SpriteColor;
+
+		// Add Children to this Object.
+		minimap.transform.parent = this.transform;
+
+		// Transform Position
+		minimap.transform.localPosition = Vector3.zero;
+		minimap.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+	}
+
+	/// <summary>
+	/// Called once per frame.
+	/// </summary>
+	void Update () 
+	{
+
+	}
+	#endregion MonoBehaviour Methods
+}
