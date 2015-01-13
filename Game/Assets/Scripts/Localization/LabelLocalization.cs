42d65e4776f42e21078e68c9be76fb10cbbd415e
Diogo Muller <diogo.muller@lactec.org.br>
2015-01-13 11:32:43 -0200

    Centralized localization between game and title screen.

---
+++ Game/Assets/Scripts/Localization/LabelLocalization.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class LabelLocalization : MonoBehaviour 
+{
+	/// <summary>
+	/// Use this for initialization.
+	/// </summary>
+	void Start() 
+	{
+		var text = GetComponent<Text>();
+		text.text = LocalizationManager.GetText(text.text);
+	}
+}
