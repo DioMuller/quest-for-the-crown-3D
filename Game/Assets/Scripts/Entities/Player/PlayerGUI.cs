5c8c4bc36c45be11c50eca2452b97db73bf5460e
Diogo Muller <diogo.muller@lactec.org.br>
2015-01-21 11:31:16 -0200

    Weapon GUI.

--- Game/Assets/Scripts/Entities/Player/PlayerGUI.cs
+++ Game/Assets/Scripts/Entities/Player/PlayerGUI.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 
 [RequireComponent(typeof(CharacterStatus))]
+[RequireComponent(typeof(PlayerWeapons))]
 public class PlayerGUI : MonoBehaviour 
 {
 	#region Public Attributes
@@ -12,15 +13,33 @@ public class PlayerGUI : MonoBehaviour
 	public Canvas PlayerCanvas;
 	#endregion Public Attributes
 
-	#region Private Attributes (Player Attributes)
+	#region Private Attributes (Player Component Instances)
 	/// <summary>
 	/// The health script instance.
 	/// </summary>
 	private CharacterStatus _characterStatus;
-	#endregion Private Attributes (Player Attributes)
 
-	#region Private Attributes (GUI Attributes)
-	/// <summary>
+    /// <summary>
+    /// Player weapons instance.
+    /// </summary>
+    private PlayerWeapons _playerWeapons;
+    #endregion Private Attributes (Player Component Instances)
+
+    #region Private Attributes (Auxiliary Attributes)
+    /// <summary>
+    /// Primary Weapon Index.
+    /// </summary>
+    private int _primaryWeaponIndex = -1;
+
+    /// <summary>
+    /// Secondary Weapon Index.
+    /// </summary>
+    private int _secondaryWeaponIndex = -1;
+    #endregion Private Attributes (Auxiliary Attributes)
+
+
+    #region Private Attributes (GUI Attributes)
+    /// <summary>
 	/// The health bar instance.
 	/// </summary>
 	private CircleBar _healthBar;
@@ -29,6 +48,16 @@ public class PlayerGUI : MonoBehaviour
 	/// The magic bar instance.
 	/// </summary>
 	private CircleBar _magicBar;
+
+    /// <summary>
+    /// Primary Weapon GUI Renderer.
+    /// </summary>
+    private Image _primaryWeapon;
+
+    /// <summary>
+    /// Secondary weapon GUI renderer.
+    /// </summary>
+    private Image _secondaryWeapon;
 	#endregion Private Attributes (GUI Attributes)
 
 	#region MonoBehaviour Methods
@@ -39,6 +68,7 @@ public class PlayerGUI : MonoBehaviour
 	{
 		#region Player Scripts
 		_characterStatus = GetComponent<CharacterStatus>();
+        _playerWeapons = GetComponent<PlayerWeapons>();
 		#endregion Player Scripts
 
 		#region GUI Components
@@ -46,7 +76,8 @@ public class PlayerGUI : MonoBehaviour
 		{
 			var statusPanel = PlayerCanvas.transform.FindChild("StatusPanel");
 
-			var lifeBar = statusPanel.transform.FindChild("LifeBar");
+            #region Life and Magic bars
+            var lifeBar = statusPanel.transform.FindChild("LifeBar");
 			var magicBar = statusPanel.transform.FindChild("MagicBar");
 
 			_healthBar = lifeBar.GetComponent<CircleBar>();
@@ -57,7 +88,13 @@ public class PlayerGUI : MonoBehaviour
 
             _magicBar.MaximumValue = _characterStatus.Data.MaxMagic;
 			_magicBar.CurrentValue = _characterStatus.CurrentMagic;
-		}
+            #endregion Life and Magic bars
+
+            #region Equiped Weapons
+            _primaryWeapon = statusPanel.transform.FindChild("PrimaryItem").FindChild("Item").GetComponent<Image>();
+            _secondaryWeapon = statusPanel.transform.FindChild("SecondaryItem").FindChild("Item").GetComponent<Image>();
+            #endregion Equiped Weapons
+        }
 		#endregion GUI Components
 	}
 	
@@ -73,6 +110,47 @@ public class PlayerGUI : MonoBehaviour
 		// Magic
 		if( _characterStatus.CurrentMagic != _magicBar.CurrentValue ) _magicBar.CurrentValue = _characterStatus.CurrentMagic;
         if (_characterStatus.Data.MaxMagic != _magicBar.MaximumValue) _magicBar.MaximumValue = _characterStatus.Data.MaxMagic;
+
+        // Primary Weapon
+        if( _playerWeapons.GetWeaponIndex(0) != _primaryWeaponIndex )
+        {
+            _primaryWeaponIndex = _playerWeapons.GetWeaponIndex(0);
+
+            if (_primaryWeaponIndex >= 0)
+            {
+                var weapon = _playerWeapons.GetWeapon(0);
+                _primaryWeapon.enabled = true;
+                _primaryWeapon.sprite = Sprite.Create(weapon.Data.Icon,
+                    new Rect(0, 0, weapon.Data.Icon.width, weapon.Data.Icon.height),
+                    new Vector2(0.5f, 0.5f), weapon.Data.Icon.height);
+            }
+            else
+            {
+                _primaryWeapon.enabled = false;
+            }
+        }
+
+        // Secondary Weapon
+        if (_playerWeapons.GetWeaponIndex(1) != _secondaryWeaponIndex)
+        {
+            _secondaryWeaponIndex = _playerWeapons.GetWeaponIndex(1);
+
+            if (_secondaryWeaponIndex >= 0)
+            {
+                var weapon = _playerWeapons.GetWeapon(1);
+                _secondaryWeapon.enabled = true;
+                _secondaryWeapon.sprite = Sprite.Create(weapon.Data.Icon,
+                    new Rect(0, 0, weapon.Data.Icon.width, weapon.Data.Icon.height),
+                    new Vector2(0.5f, 0.5f), weapon.Data.Icon.height);
+            }
+            else
+            {
+                _secondaryWeapon.enabled = false;
+            }
+        }
+
+
+
 	}
 	#endregion MonoBehaviour Methods
 }
