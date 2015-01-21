9af78ed3afdc519c8d7635bb35124d8d398029ca
Diogo Muller <diogo.muller@lactec.org.br>
2015-01-21 16:00:56 -0200

    Map simple navigation (for tests)

---
+++ Game/Assets/Scripts/Navigation/Teleporter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour 
+{
+    public string Map;
+    public string Waypoint;
+
+	void OnTriggerEnter(Collider other)
+    {
+        if( other.tag == "Player" )
+        {
+            Application.LoadLevel(Map);
+        }
+    }
+}
