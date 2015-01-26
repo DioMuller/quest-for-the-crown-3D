8618f72ebf9b91ab5d7110c455bc0341f7f7b4c3
Joao Vitor P. Moraes <jvlppm@gmail.com>
2015-01-26 00:02:23 -0200

    Bow and Arrow weapon.

---
+++ Game/Assets/Scripts/Weapons/Arrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : WeaponHitbox 
+{
+    private Rigidbody _rigidbody;
+
+    public Vector3 Direction = new Vector3(0, 0, -1);
+    public float Speed = 300.0f;
+
+    public void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        _rigidbody.MovePosition(transform.position + (Direction * Time.fixedDeltaTime * Speed));
+    }
+}
---
+++ Game/Assets/Scripts/Weapons/BowAndArrow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BowAndArrow : Weapon 
+{
+	/// <summary>
+	/// Raises the attack event.
+	/// </summary>
+    public override void OnAttack(WeaponHitbox hitbox)
+	{
+        var hb = hitbox as Arrow;
+
+        if (hb != null)
+        {
+            hb.Direction = hb.Direction.RotateY(hb.transform.localRotation.eulerAngles.y);
+        }
+	}
+
+    /// <summary>
+    /// Called when the attack fails.
+    /// </summary>
+    public override void OnAttackFail()
+    {
+        
+    }
+
+	/// <summary>
+	/// Raises the equip event.
+	/// </summary>
+    public override void OnEquip()
+	{
+
+	}
+
+	/// <summary>
+	/// Raises the unequip event.
+	/// </summary>
+	public override void OnUnequip()
+	{
+
+	}
+
+    /// <summary>
+    /// Can the weapon attack right now?
+    /// </summary>
+    /// <returns>If the weapon can attack.</returns>
+    public override bool CanAttack()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Called when the hitbox is destroyed.
+    /// </summary>
+    public override void OnHitboxDestroyed()
+    {
+        
+    }
+}
