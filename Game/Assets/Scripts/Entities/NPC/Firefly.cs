76772d65758203ee58b1af4913ab8f7f5f423eba
Diogo Muller <diogo.muller@lactec.org.br>
2015-02-03 10:28:46 -0200

    Test torch light.

--- Game/Assets/Scripts/Entities/NPC/Firefly.cs
+++ Game/Assets/Scripts/Entities/NPC/Firefly.cs
@@ -6,6 +6,7 @@ public class Firefly : MonoBehaviour
     private float[] _hsl;
     private float _currentValue = 0.0f;
     private float _initialIntensity;
+    private Vector3 _basePosition;
 
     public Light FireflyLight;
 
@@ -13,6 +14,12 @@ public class Firefly : MonoBehaviour
 
     public float IntensityChange = 0.5f;
     public float IntensityCyclesPerSecond = 1.0f;
+
+    public Vector3 WanderDistance = new Vector3(10.0f, 0.5f, 10.0f);
+
+    public bool AllowMovement = false;
+    public bool AllowColorChange = false;
+    public bool AllowIntensityChange = true;
 
     void Start()
     {
@@ -64,6 +71,7 @@ public class Firefly : MonoBehaviour
             #endregion HSL
 
             _initialIntensity = FireflyLight.intensity;
+            _basePosition = transform.position;
         }
 
     }
@@ -72,61 +80,78 @@ public class Firefly : MonoBehaviour
     {
         if( FireflyLight != null )
         {
-            #region Hue Change
-            _hsl[0] += (HueChange * Time.deltaTime);
-            if (_hsl[0] >= 360.0f) _hsl[0] -= 360.0f;
+            if (AllowColorChange)
+            {
+                #region Hue Change
+                _hsl[0] += (HueChange * Time.deltaTime);
+                if (_hsl[0] >= 360.0f) _hsl[0] -= 360.0f;
 
-            var C = (1 - Mathf.Abs(2 * _hsl[2] - 1)) * _hsl[1];
-            var X = C * (1 - Mathf.Abs(((_hsl[0] / 60.0f) % 2) - 1));
-            var m = _hsl[2] - C / 2.0f;
+                var C = (1 - Mathf.Abs(2 * _hsl[2] - 1)) * _hsl[1];
+                var X = C * (1 - Mathf.Abs(((_hsl[0] / 60.0f) % 2) - 1));
+                var m = _hsl[2] - C / 2.0f;
 
-            var rl = 0.0f;
-            var gl = 0.0f;
-            var bl = 0.0f;
+                var rl = 0.0f;
+                var gl = 0.0f;
+                var bl = 0.0f;
 
-            if (0.0f <= _hsl[0] && _hsl[0] < 60.0f)
-            {
-                rl = C;
-                gl = X;
-                bl = 0.0f;
-            }
-            else if (60.0f <= _hsl[0] && _hsl[0] < 120.0f)
-            {
-                rl = X;
-                gl = C;
-                bl = 0.0f;
-            }
-            else if (120.0f <= _hsl[0] && _hsl[0] < 180.0f)
-            {
-                rl = 0.0f;
-                gl = C;
-                bl = X;
-            }
-            else if (180.0f <= _hsl[0] && _hsl[0] < 240.0f)
-            {
-                rl = 0.0f;
-                gl = X;
-                bl = C;
-            }
-            else if (240.0f <= _hsl[0] && _hsl[0] < 300.0f)
-            {
-                rl = X;
-                gl = 0.0f;
-                bl = C;
-            }
-            else if (300.0f <= _hsl[0] && _hsl[0] < 360.0f)
-            {
-                rl = C;
-                gl = 0.0f;
-                bl = X;
+                if (0.0f <= _hsl[0] && _hsl[0] < 60.0f)
+                {
+                    rl = C;
+                    gl = X;
+                    bl = 0.0f;
+                }
+                else if (60.0f <= _hsl[0] && _hsl[0] < 120.0f)
+                {
+                    rl = X;
+                    gl = C;
+                    bl = 0.0f;
+                }
+                else if (120.0f <= _hsl[0] && _hsl[0] < 180.0f)
+                {
+                    rl = 0.0f;
+                    gl = C;
+                    bl = X;
+                }
+                else if (180.0f <= _hsl[0] && _hsl[0] < 240.0f)
+                {
+                    rl = 0.0f;
+                    gl = X;
+                    bl = C;
+                }
+                else if (240.0f <= _hsl[0] && _hsl[0] < 300.0f)
+                {
+                    rl = X;
+                    gl = 0.0f;
+                    bl = C;
+                }
+                else if (300.0f <= _hsl[0] && _hsl[0] < 360.0f)
+                {
+                    rl = C;
+                    gl = 0.0f;
+                    bl = X;
+                }
+
+                var newColor = new Color(rl + m, gl + m, bl + m);
+                FireflyLight.color = newColor;
+                #endregion Hue Change
             }
 
-            var newColor = new Color(rl + m, gl + m, bl + m);
-            FireflyLight.color = newColor;
-            #endregion Hue Change
+            if (AllowIntensityChange || AllowMovement)
+            {
+                _currentValue += (IntensityCyclesPerSecond * Time.deltaTime);
 
-            _currentValue += (IntensityCyclesPerSecond * Time.deltaTime);
-            FireflyLight.intensity = _initialIntensity + (Mathf.Cos(Mathf.PI * _currentValue) * IntensityChange);
+                if (AllowIntensityChange)
+                {
+                    FireflyLight.intensity = _initialIntensity + (Mathf.Cos(Mathf.PI * _currentValue) * IntensityChange);
+                }
+
+                if (AllowMovement)
+                {
+                    transform.position = _basePosition + new Vector3(WanderDistance.x * Mathf.Sin(_currentValue),
+                                                                WanderDistance.y * Mathf.Tan(_currentValue),
+                                                                WanderDistance.z * Mathf.Cos(_currentValue));
+                }
+            }
         }
     }
 }
