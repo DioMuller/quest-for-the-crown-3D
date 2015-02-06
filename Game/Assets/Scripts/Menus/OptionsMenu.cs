8a1fa7bf0369f3ba1dca1dff92ae80181064daab
Diogo Muller <diogo.muller@lactec.org.br>
2015-02-06 15:53:28 -0200

    WIP: Input methods options.

--- Game/Assets/Scripts/Menus/OptionsMenu.cs
+++ Game/Assets/Scripts/Menus/OptionsMenu.cs
@@ -20,18 +20,26 @@ public class ComboOption
 public class OptionsMenu : MonoBehaviour 
 {
 	#region Public Attributes
+    // General
 	public Text LanguageLabel;
+    // Controls
+    public Text Player1InputLabel;
+    public Text Player2InputLabel;
 	#endregion Public Attributes
 
 	#region Private Attributes
 	private int _selectedLanguage = 0;
 	private ComboOption[] _languages;
+    private int _selectedControlPlayer1 = 0;
+    private ComboOption[] _controlMethodsP1;
+    private int _selectedControlPlayer2 = 1;
+    private ComboOption[] _controlMethodsP2;
 	#endregion Private Attributes
 
 	/// <summary>
 	/// Awake is called on Initialization.
 	/// </summary>
-	void Awake()
+	void Start()
 	{
 		#region Load Languages
 		_languages = new ComboOption[]
@@ -52,8 +60,34 @@ public class OptionsMenu : MonoBehaviour
 		}
 
 		ChangeLanguageLabel();
-		#endregion Load Languages
-	}
+		#endregion Load Inputs
+
+        #region Load Inputs P1
+        _controlMethodsP1 = new ComboOption[]
+		{
+			new ComboOption(LocalizationManager.GetText("Options.Keyboard"), "Keyboard"),
+            new ComboOption(LocalizationManager.GetText("Options.Controller"), "Joy1"),
+			new ComboOption(LocalizationManager.GetText("Options.KeyboardController"), "Keyboard;Joy1")
+		};
+
+        currentSelected = _controlMethodsP1.FirstOrDefault((opt) => opt.Value == GameSettings.Player1Input);
+
+        if (currentSelected == null)
+        {
+            _selectedControlPlayer1 = 0;
+        }
+        else
+        {
+            _selectedControlPlayer1 = Array.IndexOf(_controlMethodsP1, currentSelected);
+        }
+
+        ChangeControlPlayer1Label();
+        #endregion Load Inputs P1
+
+        #region Load Inputs P2
+        LoadPlayer2Options();
+        #endregion Load Inputs P2
+    }
 
 	#region Event Handlers
 	public void OnSaveClick()
@@ -65,7 +99,19 @@ public class OptionsMenu : MonoBehaviour
 		}
 		#endregion Language
 
-		Application.LoadLevel("Title");
+        #region Input Methods
+        if (Player1InputLabel != null && _selectedControlPlayer1 < _controlMethodsP1.Length)
+        {
+            GameSettings.Player1Input = _controlMethodsP1[_selectedControlPlayer1].Value;
+        }
+
+        if (Player2InputLabel != null && _selectedControlPlayer2 < _controlMethodsP2.Length)
+        {
+            GameSettings.Player2Input = _controlMethodsP2[_selectedControlPlayer2].Value;
+        }
+        #endregion Input Methods
+
+        Application.LoadLevel("Title");
 	}
 
 	public void OnCancelClick()
@@ -73,11 +119,24 @@ public class OptionsMenu : MonoBehaviour
 		Application.LoadLevel("Title");
 	}
 
-	public void OnLanguageClick()
+    public void OnLanguageClick()
+    {
+        _selectedLanguage = (_selectedLanguage + 1) % _languages.Length;
+        ChangeLanguageLabel();
+    }
+
+	public void OnP1InputClick()
 	{
-		_selectedLanguage = (_selectedLanguage + 1) % _languages.Length;
-		ChangeLanguageLabel();
+        _selectedControlPlayer1 = (_selectedControlPlayer1 + 1) % _controlMethodsP1.Length;
+		ChangeControlPlayer1Label();
+        LoadPlayer2Options();
 	}
+
+    public void OnP2InputClick()
+    {
+        _selectedControlPlayer2 = (_selectedControlPlayer2 + 1) % _controlMethodsP2.Length;
+        ChangeControlPlayer2Label();
+    }
 	#endregion EventHandlers
 
 	#region Helper Methods
@@ -91,5 +150,57 @@ public class OptionsMenu : MonoBehaviour
 			LanguageLabel.text = _languages[_selectedLanguage].Label;
 		}
 	}
+
+    private void ChangeControlPlayer1Label()
+    {
+        if( Player1InputLabel != null && _selectedControlPlayer1 < _controlMethodsP1.Length )
+        {
+            Player1InputLabel.text = _controlMethodsP1[_selectedControlPlayer1].Label;
+        }
+    }
+
+    private void ChangeControlPlayer2Label()
+    {
+        if (Player2InputLabel != null && _selectedControlPlayer2 < _controlMethodsP2.Length)
+        {
+            Player2InputLabel.text = _controlMethodsP2[_selectedControlPlayer2].Label;
+        }
+    }
+
+    private void LoadPlayer2Options()
+    {
+        var inputsP1 = _controlMethodsP1[_selectedControlPlayer1].Value.Split(';');
+        string joy = inputsP1.Contains("Joy1") ? "Joy2" : "Joy1";
+
+        if( !inputsP1.Contains("Keyboard") )
+        {
+            _controlMethodsP2 = new ComboOption[]
+		    {
+			    new ComboOption(LocalizationManager.GetText("Options.Keyboard"), "Keyboard"),
+                new ComboOption(LocalizationManager.GetText("Options.Controller"), joy),
+			    new ComboOption(LocalizationManager.GetText("Options.KeyboardController"), "Keyboard;" + joy)
+		    };
+        }
+        else
+        {
+            _controlMethodsP2 = new ComboOption[]
+		    {
+                new ComboOption(LocalizationManager.GetText("Options.Controller"), joy)
+		    };
+        }
+
+        var currentSelected = _controlMethodsP2.FirstOrDefault((opt) => opt.Value == GameSettings.Player2Input);
+
+        if (currentSelected == null)
+        {
+            _selectedControlPlayer2 = 0;
+        }
+        else
+        {
+            _selectedControlPlayer2 = Array.IndexOf(_controlMethodsP2, currentSelected);
+        }
+
+        ChangeControlPlayer2Label();
+    }
 	#endregion Helper Methods
 }
--- Game/Assets/Scripts/Settings/GameSettings.cs
+++ Game/Assets/Scripts/Settings/GameSettings.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public static class GameSettings
-{ 
-	public static string Language
+{
+    #region General Options
+    public static string Language
 	{
 		get
 		{
@@ -15,5 +16,36 @@ public static class GameSettings
 		{
 			PlayerPrefs.SetString("Language", value);
 		}
-	}
+    }
+    #endregion General Options
+
+    #region Controller Options
+    public static string Player1Input
+    {
+        get
+        {
+            if (!PlayerPrefs.HasKey("Player1Input")) return "Keyboard;Joy1";
+
+            return PlayerPrefs.GetString("Player1Input");
+        }
+        set
+        {
+            PlayerPrefs.SetString("Player1Input", value);
+        }
+    }
+
+    public static string Player2Input
+    {
+        get
+        {
+            if (!PlayerPrefs.HasKey("Player2Input")) return "Joy2";
+
+            return PlayerPrefs.GetString("Player2Input");
+        }
+        set
+        {
+            PlayerPrefs.SetString("Player2Input", value);
+        }
+    }
+    #endregion Controller Options
 }
