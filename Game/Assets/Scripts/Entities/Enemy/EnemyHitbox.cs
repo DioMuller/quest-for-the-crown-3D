c19d04c12e7e66bc2234ad8a10c879232ffe1a57
Diogo Muller <diogo.muller@lactec.org.br>
2015-02-09 14:47:53 -0200

    Enemy hits remove player health.

---
+++ Game/Assets/Scripts/Entities/Enemy/EnemyHitbox.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EnemyHitbox : MonoBehaviour 
+{
+    public string[] Targets = { "Player", "Enemy" };
+    public int AttackPower = 1;
+
+	protected void OnCollisionEnter(Collision other)
+	{
+        if (Targets.Contains(other.transform.tag))
+        {
+            var status = other.gameObject.GetComponent<CharacterStatus>();
+
+            OnHit(status);
+        }
+	}
+
+	public bool OnHit(CharacterStatus other)
+	{
+		if(other == null) return false;
+		
+		other.RemoveHealth( AttackPower, transform );
+		return true;
+	}
+}
