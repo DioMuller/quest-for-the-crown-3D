318c1c414e7acd17cd501211a3cd4c1ec36f98ea
Joao Vitor P. Moraes <jvlppm@gmail.com>
2015-02-11 23:52:00 -0200

    Simple HealthItem prefab.

---
+++ Game/Assets/Scripts/Items/HealthItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HealthItem : ItemBase
+{
+    public int Ammount = 1;
+
+    protected override void OnPlayerTouch(Collider player)
+    {
+        var status = player.GetComponent<CharacterStatus>();
+        if (status != null)
+        {
+            status.AddHealth(Ammount);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Game/Assets/Scripts/Items/ItemBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ItemBase : MonoBehaviour
+{
+
+    /// <summary>
+    /// Called when any objects collides with this.
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            OnPlayerTouch(other);
+        }
+    }
+
+    protected abstract void OnPlayerTouch(Collider player);
+}
