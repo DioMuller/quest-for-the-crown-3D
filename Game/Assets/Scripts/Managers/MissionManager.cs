429e3a04c5a6262ffbf26d54400666bc365bc4da
Diogo Muller <diogo.muller@lactec.org.br>
2015-03-03 13:52:52 -0300

    WIP: Mission data structure.

---
+++ Game/Assets/Scripts/Managers/MissionManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class MissionInfo
+{
+    // Level Info
+    public string Title;
+    public string Description;
+    public Texture2D Image;
+    // Level Data
+    public string Scene;
+}
+
+public class MissionManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
