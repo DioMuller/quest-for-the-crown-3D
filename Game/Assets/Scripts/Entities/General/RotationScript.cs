25f9c3ff41524f7edf5172abd37f4adbd738560b
Diogo Muller <diogo.muller@lactec.org.br>
2015-03-11 13:32:15 -0300

    Boomerang rotation script.

---
+++ Game/Assets/Scripts/Entities/General/RotationScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour 
+{
+    public float RotationAmount = 1.0f;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Rotate(Vector3.forward, RotationAmount * Time.deltaTime);
+	}
+}
