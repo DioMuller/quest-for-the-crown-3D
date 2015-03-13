d0b5a1d51d9f0427517ce086f9778deaf227698f
Diogo Muller <diogo.muller@lactec.org.br>
2015-03-13 16:09:48 -0300

    Hard-coded item and weapon count/scripts.

--- Game/Assets/Scripts/Managers/PlayerManager.cs
+++ Game/Assets/Scripts/Managers/PlayerManager.cs
@@ -3,9 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
+public enum Items
+{
+    HealthPotion,
+    MagicPotion,
+    Bomb,
+    Medal
+}
+
 public class PlayerManager : SingletonBehaviour<PlayerManager>
 {
     private static int playerCount = 1;
+
+    #region Weapons and Items
+    // Hard-Coded for time constrains.
+    public bool HasBow { get; private set; }
+    public bool HasFireball { get; private set; }
+    public bool HasBoomerang { get; private set; }
+
+    public int HealthPotions { get; private set; }
+    public int MagicPotions { get; private set; }
+    public int Bombs { get; private set; }
+    public int Medals { get; private set; }
+    #endregion Weapons and Items
 
     private List<PlayerRegister> _players = new List<PlayerRegister>();
 
@@ -28,5 +48,44 @@ public class PlayerManager : SingletonBehaviour<PlayerManager>
     {
         if (playerCount >= 2) return;
         else playerCount++;
+    }
+
+    public void ResetGame()
+    {
+        playerCount = 1;
+
+        HasBow = false;
+        HasFireball = false;
+        HasBoomerang = false;
+
+        HealthPotions = 0;
+        MagicPotions = 0;
+        Bombs = 0;
+        Medals = 0;
+    }
+
+    public bool UseItem(Items item)
+    {
+        switch(item)
+        {
+            case Items.HealthPotion:
+                if (HealthPotions <= 0) return false;
+                HealthPotions--;
+                return true;
+            case Items.MagicPotion:
+                if (MagicPotions <= 0) return false;
+                MagicPotions--;
+                return true;
+            case Items.Bomb:
+                if (Bombs <= 0) return false;
+                Bombs--;
+                return true;
+            case Items.Medal:
+                if (Medals <= 0) return false;
+                Medals--;
+                return true;
+            default:
+                return false;
+        }
     }
 }
