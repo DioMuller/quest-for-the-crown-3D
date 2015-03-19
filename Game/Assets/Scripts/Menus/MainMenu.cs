2eb2861f90ed53c0787396a97e7459882f22ecdc
Diogo Muller <diogo.muller@live.com>
2015-03-18 21:15:49 -0300

    Redone main/options menu on single scene.

--- Game/Assets/Scripts/Menus/MainMenu.cs
+++ Game/Assets/Scripts/Menus/MainMenu.cs
@@ -1,10 +1,75 @@
+using UnityEditor;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour 
+public class MainMenu : MonoBehaviour
 {
-    public string NewGameScene = "";
+	#region Public Attributes
+	public string NewGameScene = "";
 
+	public Canvas MenuCanvas = null;
+	public Canvas OptionsCanvas = null;
+	
+	// General
+	public ComboBox ComboLanguage;
+
+	// Input
+	public ComboBox ComboPlayer1Input;
+
+	#endregion Public Attributes
+
+	void Awake()
+	{
+		OptionsCanvas.enabled = false;
+	}
+
+	#region Event Handlers
+	public void OnSaveClick()
+	{
+		#region Language
+		if (ComboLanguage != null)
+		{
+			GameSettings.Language = ComboLanguage.SelectedValue;
+		}
+		#endregion Language
+
+		#region Input Methods
+		if (ComboPlayer1Input != null)
+		{
+			string p1Input = ComboPlayer1Input.SelectedValue;
+			string p2Input = "";
+
+			GameSettings.Player1Input = p1Input;
+
+			if (!p1Input.Contains("Joy1"))
+			{
+				p2Input = "Joy1";
+			}
+			else if (!p1Input.Contains("Keyboard"))
+			{
+				p2Input = "Keyboard";
+			}
+			else
+			{
+				p2Input = "Joy2";
+			}
+
+			GameSettings.Player2Input = p2Input;
+		}
+		#endregion Input Methods
+
+		OnCancelClick();
+	}
+
+	public void OnCancelClick()
+	{
+		MenuCanvas.enabled = true;
+		OptionsCanvas.enabled = false;
+
+		MenuCanvas.GetComponentInChildren<Button>().Select();
+	}
+	
 	public void OnNewGameClick()
 	{
 		PlayerManager.ResetGame();
@@ -17,6 +82,10 @@ public class MainMenu : MonoBehaviour
 
 	public void OptionsClick()
 	{
-		Application.LoadLevel("Options");
+		MenuCanvas.enabled = false;
+		OptionsCanvas.enabled = true;
+
+		OptionsCanvas.GetComponentInChildren<Button>().Select();
 	}
+	#endregion EventHandlers
 }
