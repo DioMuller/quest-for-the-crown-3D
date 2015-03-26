7feefcadb8c4e1f936fd84cba91a7d9313d29fe5
Diogo Muller <diogo.muller@lactec.org.br>
2015-03-26 16:47:22 -0300

    Weapon trail on both player prefabs.

---
+++ Game/Assets/Scripts/Effects/TrailEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrailEffect : MonoBehaviour 
+{
+    public GameObject Trail;
+
+    private Stack<GameObject> _activeTrails = new Stack<GameObject>();
+
+    void OnEnable()
+    {
+        var trail = Instantiate(Trail);
+        trail.transform.parent = transform;
+        trail.transform.localPosition = Vector3.zero;
+        trail.transform.localEulerAngles = Vector3.zero;
+
+        _activeTrails.Push(trail);
+    }
+
+    void OnDisable()
+    {
+        while(_activeTrails.Count > 0)
+        {
+            Destroy(_activeTrails.Pop());
+        }
+    }
+}
